980da0833b432796f49762e0fff6470572ae108d
Vladyslav <124316302+VMensem@users.noreply.github.com>
2025-09-30 20:36:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
     
 @app.route('/login')
-def home():
+def login():
     return render_template('login.html')
     
 if __name__ == '__main__':
