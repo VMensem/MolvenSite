b6ffb900aa580f02024612892a2c7c3c7cccdef2
Vladyslav <124316302+VMensem@users.noreply.github.com>
2025-10-20 05:08:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
